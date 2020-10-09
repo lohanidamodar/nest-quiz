567e3ae4b8e205c8edc4fdf1bc7c1014526104d5
Damodar Lohani <dlohani48@gmail.com>
2020-10-09 22:46:17 +0545

    config and database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as config from 'config';
+const db: any = config.get('db');
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(db.uri)],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
+  const serverConfig:any = config.get('server');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || serverConfig.port;
+  await app.listen(port);
 }
 bootstrap();
